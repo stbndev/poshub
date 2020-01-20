411e25260f3140dae2b51c05bb40a1e7d303c5d9
STBN <25061425+stbn200@users.noreply.github.com>
2020-01-20 00:30:54 -0600

    tsdada

---
+++ code/Frontend/my-dream-app/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainmenuComponent } from './mainmenu/mainmenu.component';
+
+const routes: Routes = [
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
