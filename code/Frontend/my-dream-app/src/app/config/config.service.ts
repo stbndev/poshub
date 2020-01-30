ee565a98f7fe80bf70e906306f5ece39bedc1ad1
STBN <25061425+stbn200@users.noreply.github.com>
2020-01-30 11:55:48 -0600

    test

--- code/Frontend/my-dream-app/src/app/config/config.service.ts
+++ code/Frontend/my-dream-app/src/app/config/config.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+import { Tipos } from "./tipos.enum";
 
 @Injectable({
   providedIn: 'root'
@@ -37,5 +37,24 @@ export class ConfigService {
 
   Post(serviceName:String,data:any):Observable<any> {
     return this.http.post(`${this.uriResources}${serviceName}`,data).pipe(map(this.extractData));
+  }
+
+  Make(serviceName:String,tipo:Tipos , data:any):Observable<any> {
+    switch (tipo) {
+      case Tipos.POST:
+        return this.http.post(`${this.uriResources}${serviceName}`,data).pipe(map(this.extractData));
+        break;
+
+        case Tipos.PATCH:
+        return this.http.patch(`${this.uriResources}${serviceName}`,data).pipe(map(this.extractData));
+        break;
+
+        case Tipos.PATCH:
+          return this.http.patch(`${this.uriResources}${serviceName}`,data).pipe(map(this.extractData));
+          break;
+    
+      default:
+        break;
+    }
   }
 }
