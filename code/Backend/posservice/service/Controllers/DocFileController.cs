873c3cd0b01cca39811433c1885d1ae47165817b
STBN <25061425+stbndev@users.noreply.github.com>
2020-02-04 12:25:10 -0600

    jjkjkkjkj

--- code/Backend/posservice/service/Controllers/DocFileController.cs
+++ code/Backend/posservice/service/Controllers/DocFileController.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using posrepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
-
+using System.IO;
+using System.Web.Http;
 namespace service.Controllers
 {
     public class DocFileController : ApiController
@@ -14,21 +16,41 @@ namespace service.Controllers
         {
             HttpResponseMessage result = null;
             var httpRequest = HttpContext.Current.Request;
-            if (httpRequest.Files.Count > 0)
+            string tmpname = PosUtil.ConvertToTimestamp(DateTime.Now).ToString();
+            try
             {
-                var docfiles = new List<string>();
-                foreach (string file in httpRequest.Files)
+
+
+                if (httpRequest.Files.Count > 0)
                 {
-                    var postedFile = httpRequest.Files[file];
-                    var filePath = HttpContext.Current.Server.MapPath("~/" + postedFile.FileName);
-                    postedFile.SaveAs(filePath);
-                    docfiles.Add(filePath);
+                    var docfiles = new List<string>();
+                    foreach (string file in httpRequest.Files)
+                    {
+
+                        var postedFile = httpRequest.Files[file];
+                        //var filePath = HttpContext.Current.Server.MapPath("~/images/" + postedFile.FileName);
+                        tmpname = tmpname + Path.GetExtension(postedFile.FileName);
+                        //var filePath = HttpContext.Current.Server.MapPath("~/images/" + tmpname);
+                        //postedFile.SaveAs(filePath);
+
+                        string fname = Path.GetFileName(postedFile.FileName);
+                        string filePath = HttpContext.Current.Server.MapPath(Path.Combine("~/images", fname));
+                        postedFile.SaveAs(filePath);
+                        docfiles.Add(filePath);
+
+                    }
+                    result = Request.CreateResponse(HttpStatusCode.Created, docfiles);
                 }
-                result = Request.CreateResponse(HttpStatusCode.Created, docfiles);
+                else
+                {
+                    result = Request.CreateResponse(HttpStatusCode.BadRequest);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+
+                result = Request.CreateResponse(HttpStatusCode.Conflict, ex.Message + ex.InnerException.Message);
+
             }
             return result;
         }
